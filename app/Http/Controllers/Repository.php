fcf063711b5766f371392dedc336e7dea8abd94f
Andrew Johnson <666617+Ehaic@users.noreply.github.com>
2022-08-23 11:14:34 -0500

    Added refresh button.

--- app/Http/Controllers/Repository.php
+++ app/Http/Controllers/Repository.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 class Repository extends Controller
 {
+    //Display all repo's
     public function index() {
-        $repositories = \App\Models\Repository::orderBy('stars', 'desc')->get();
+        $repositories = \App\Models\Repository::orderBy('stars', 'desc')->get(['projectName', 'id', 'stars']);
         return view('index')->with('repositories', $repositories);
     }
 
-    public function getRepoDetails($id): array {
+    //Get Repo Details from ID
+    public function getRepoDetails($id) {
         $repository = \App\Models\Repository::find($id);
         $success = !is_null($repository);
        return [
@@ -22,32 +25,40 @@ class Repository extends Controller
     }
 
     //Update the repositories in the database from the GitHub API Gets top 100 starred Repositories.
-    public function updateRepositories(): string {
+    public function updateRepositories() {
+        //Search API Repositories endpoint.
         $githubApi = Http::get('https://api.github.com/search/repositories',
         [
-            'q' => 'language:php', //Repositories with PHP language tag
+            'q' => 'language:php', //PHP language tag
             'sort' => 'stars', //Sort results by number of stars,
             'order' => 'desc', //highest to lowest
             'per_page' => '100', //100 results per page
             'page' => '1' //first page
         ]);
         $searchResults = json_decode($githubApi->body());
-        //foreach search result
-        foreach ($searchResults->items as $repository) {
-            //make a new model, or update if exists
-            $localRepository = \App\Models\Repository::updateOrCreate([
-                'id' => $repository->id
-            ], [
-                'id' => $repository->id,
-                'projectName' => $repository->name,
-                'URL' => $repository->html_url,
-                'repositoryCreatedAt' => \Carbon\Carbon::parse($repository->created_at),
-                'repositoryLastPush' => \Carbon\Carbon::parse($repository->pushed_at),
-                'description' => empty($repository->description) ? 'No Description' : $repository->description,
-                'stars' => $repository->stargazers_count
-            ]);
+        $success = count($searchResults->items) > 0;
+        if ($success) {
+            //Truncate the table, we only store 100 results.
+            DB::table('repositories')->truncate();
+            //foreach search result
+            $bulkData = array();
+            foreach ($searchResults->items as $repository) {
+                //Map API Data to relevant Fields
+                $repoInfo  = [
+                    'id' => $repository->id,
+                    'projectName' => $repository->name,
+                    'URL' => $repository->html_url,
+                    'repositoryCreatedAt' => \Carbon\Carbon::parse($repository->created_at),
+                    'repositoryLastPush' => \Carbon\Carbon::parse($repository->pushed_at),
+                    'description' => empty($repository->description) ? 'No Description' : $repository->description,
+                    'stars' => $repository->stargazers_count
+                ];
+                $bulkData[] = $repoInfo;
+            }
+            //Bulk insert all data.
+            \App\Models\Repository::insert($bulkData);
         }
-        return "DONE";
+        return ["success" => $success];
     }
 
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,9 @@
 <div class="w-3/4 lg:w-1/2 mx-auto rounded-md bg-slate-700 shadow-lg m-20 p-10 text-center content-center">
     <h1 class="text-3xl font-bold underline m-10 text-gray-200">Top 100 PHP Github Repositories Using PHP</h1>
     <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+        <button id="updateButton" onclick="updateRepos()" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center">
+            Refresh Data
+        </button>
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -40,23 +43,40 @@
 
 
 <script>
+    //Get repo detail info, populate and show modal.
     function showDetailsRow(repoID) {
         $.get('/getRepoDetails/' + repoID, function (data) {
             if(data.success) {
-                $('#detailProjectName').text(data[0].projectName);
+                $('#detailProjectName').text("Project Name: " + data[0].projectName);
                 $('#detailUrl').attr('href', data[0].URL);
                 $('#detailUrl').text(data[0].URL);
                 $('#detailDescription').text(data[0].description);
-                $('#detailCreateDate').text(data[0].repositoryCreatedAt);
-                $('#detailPushDate').text(data[0].repositoryLastPush);
+                $('#detailCreateDate').text("Project Created: " + data[0].repositoryCreatedAt);
+                $('#detailPushDate').text("Last Push Seen: " + data[0].repositoryLastPush);
             }
             let modal = $('#repoDetailModal');
             $('#repoDetailModal').removeClass('hidden');
         });
     }
+
+    //Hit update repo's endpoint, reload page.
+    function updateRepos() {
+        $('#updateButton').text("Loading...");
+        $('#updateButton').prop('disabled', true);
+        $.get('/updateRepos', function (data) {
+            if(!data['success']) {
+              alert("Issue Getting Repositories from Github");
+            }
+        }).fail(function () {
+            alert("Issue Getting Repositories from Github");
+        }).always(function () {
+            location.reload();
+        })
+    }
 </script>
 
 <script type="module">
+    //This function hides the modal
     $('#repoDetailModal').click(function () {
         $('#repoDetailModal').addClass('hidden');
     })
